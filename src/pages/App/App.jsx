e389f3d31e8c80417b0b3ac91410926275bc1116
ccrisolo <ccrisolo3@gmail.com>
2021-07-24 13:05:10 -0700

    add user state and set up react router dom

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,28 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import NewOrderPage from "../NewOrderPage";
+import AuthPage from "../AuthPage";
+import OrderHistoryPage from "../OrderHistoryPage";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export default function App() {
-  return (
-    <main className="App">
-      App
-    </main>
-  );
-}
+    const [user, setUser] = useState({});
 
+    return (
+        <main className='App'>
+            {user ? (
+                <Switch>
+                <Route path="/orders/new">
+                  <NewOrderPage />
+                </Route>
+                <Route path="/orders">
+                  <OrderHistoryPage />
+                </Route>
+                <Redirect to='/orders' />
+              </Switch>
+            ) : (
+                <AuthPage />
+            )}
+        </main>
+    );
+}
