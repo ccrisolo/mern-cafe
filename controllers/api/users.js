041b476d6d88aff722cdea195720ea7436f9a3e8
ccrisolo <ccrisolo3@gmail.com>
2021-07-27 14:32:47 -0700

    adds create function in controller/api/users and sends back a JWT

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,4 +1,5 @@
 const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     create,
@@ -8,9 +9,26 @@ async function create(req, res) {
     try {
         //add user to the database
         const user = await User.create(req.body);
+        //create a token with the helperr function
+        //token will be a string
+        const token = createJWT(user);
+        //yes we can use res.json to send back just a string
+        //the client code takes this into consideration
+        res.json(token);
     } catch (err) {
         //Client will check for non-2xx status code
         //400 = Bad Request
         res.status(400).json(err);
     }
+}
+
+//helper functions
+
+function createJWT(user) {
+    return jwt.sign(
+        //data payload
+        { user },
+        process.env.SECRET,
+        { expiresIn: "24h" }
+    );
 }
