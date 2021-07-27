b825e1ffff835f3b9ebda387980040cb088fb5fd
ccrisolo <ccrisolo3@gmail.com>
2021-07-27 14:18:47 -0700

    adds user to database in create controller

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -4,11 +4,13 @@ module.exports = {
     create,
 };
 
-function create(req, res) {
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email,
-        },
-    });
+async function create(req, res) {
+    try {
+        //add user to the database
+        const user = await User.create(req.body);
+    } catch (err) {
+        //Client will check for non-2xx status code
+        //400 = Bad Request
+        res.status(400).json(err);
+    }
 }
