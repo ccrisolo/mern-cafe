66be6a4a6efb10cbadd85f68f29a721a26bb954b
ccrisolo <ccrisolo3@gmail.com>
2021-07-27 14:54:06 -0700

    adds token to local storage in users-service signUp function

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,13 +2,14 @@ import * as usersAPI from "./users-api";
 
 export async function signUp(userData) {
     try {
-      // Delegate the network request code to the users-api.js API module
-      // which will ultimately return a JSON Web Token (JWT)
-      const token = await usersAPI.signUp(userData);
-      // Baby step by returning whatever is sent back by the server
-      return token;
+        // Delegate the network request code to the users-api.js API module
+        // which will ultimately return a JSON Web Token (JWT)
+        const token = await usersAPI.signUp(userData);
+        //persist the 'token' in browsers local storage
+        localStorage.setItem("token", token);
+        // Baby step by returning whatever is sent back by the server
+        return token;
     } catch {
-      throw new Error('Invalid Sign Up');
+        throw new Error("Invalid Sign Up");
     }
-  }
-  
+}
